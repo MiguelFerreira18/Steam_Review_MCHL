9db8415c47d4084520d6aeac5418898cf8cccadf
MiguelFerreira18 <miguel.ferreira15@zonmail.pt>
2023-03-29 09:23:24 +0100

    Tipo das variaveis
    Descreição do tipo das variaveis

--- SteamReviewsProcessing.py
+++ SteamReviewsProcessing.py
@@ -6,27 +6,27 @@ from sklearn.preprocessing import MinMaxScaler
 
 dt = pd.read_csv('steamReviews.csv', nrows=100)
 # --------------------Descricao das colunas--------------------
-# app_id - ID do jogo
-# app_name - Nome do jogo
-# review_id - ID da review
-# language - Linguagem da review
-# review - Texto da review
-# timestamp_created - Data de criacao da review
-# timestamp_updated - Data de atualizacao da review
-# recommended - Recomendado ou nao
-# votes_helpful - Votos de utilidade
-# votes_funny - Votos de humor
-# weighted_vote_score - Media ponderada dos votos
-# comment_count - Numero de comentarios
-# steam_purchase - Comprado na Steam
-# received_for_free - Recebido de graça
-# written_during_early_access - Escrito durante o Early Access
-# author.steamid - ID do autor
-# author.num_games_owned - Numero de jogos do autor
-# author.num_reviews - Numero de reviews do autor
-# author.playtime_forever - Tempo de jogo total do autor
-# author.playtime_last_two_weeks - Tempo de jogo nas ultimas duas semanas do autor
-# author.last_played - Ultima vez que o autor jogou
+# app_id - ID do jogo, Discrete
+# app_name - Nome do jogo, Nominal
+# review_id - ID da review, Discrete
+# language - Linguagem da review, Nominal
+# review - Texto da review, Nominal
+# timestamp_created - Data de criacao da review, Discrete
+# timestamp_updated - Data de atualizacao da review, Discrete
+# recommended - Recomendado ou nao, Discrete
+# votes_helpful - Votos de utilidade, Discrete
+# votes_funny - Votos de humor, Discrete
+# weighted_vote_score - Media ponderada dos votos, Continuous
+# comment_count - Numero de comentarios, Discrete
+# steam_purchase - Comprado na Steam, Discrete
+# received_for_free - Recebido de graça, Discrete
+# written_during_early_access - Escrito durante o Early Access, Discrete
+# author.steamid - ID do autor, Discrete
+# author.num_games_owned - Numero de jogos do autor, Discrete
+# author.num_reviews - Numero de reviews do autor, Discrete
+# author.playtime_forever - Tempo de jogo total do autor, Continuous
+# author.playtime_last_two_weeks - Tempo de jogo nas ultimas duas semanas do autor, Continuous
+# author.last_played - Ultima vez que o autor jogou, Discrete
 # --------------------Descricao das colunas--------------------
 
 print(len(dt.dropna()))
