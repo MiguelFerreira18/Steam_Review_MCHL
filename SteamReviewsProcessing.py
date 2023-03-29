713c2339587a5d7cc62542767592a8e911fb41bd
MiguelFerreira18 <miguel.ferreira15@zonmail.pt>
2023-03-29 08:49:52 +0100

    librarias principais importadas

--- SteamReviewsProcessing.py
+++ SteamReviewsProcessing.py
@@ -1,1 +1,8 @@
-print("Miguel Burro")
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
+
+
+
