cef08dbc2007c74e0fd6738776f5086c01cc5a5d
Rodrigo Magalhães <rodrigomgs28@gmail.com>
2023-03-29 08:54:16 +0100

    toma miguel
    toma miguel

--- SteamReviewsProcessing.py
+++ SteamReviewsProcessing.py
@@ -1,1 +1,7 @@
-print("Miguel Burro")
+import pandas as pd
+
+dt = pd.read_csv('steamReviews.csv')
+pd.set_option('display.max_rows', 100)
+pd.set_option('display.max_columns', 10)
+
+print(dt.head())
